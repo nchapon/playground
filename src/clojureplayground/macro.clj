03ece9e666807730dd76706d41d1bc6eb6c1dd45
Nicolas Chapon <nchapon@gmail.com>
2013-08-06 17:13:29 +0200

    WIP macro

--- src/clojureplayground/macro.clj
+++ src/clojureplayground/macro.clj
@@ -26,3 +26,44 @@
      (let [now# (System/currentTimeMillis)] ;;auto-gensym
        (println "[" now# "] Call to " (str (var ~fn-name))))
      ~@body))
+
+
+(defn build-contract [c]
+  (let [args (first c)]
+    (list
+     (into '[f] c)
+     (apply merge
+            (for [con (rest c)]
+              (cond
+               (= (first con) 'require)
+               (assoc {} :pre (vec (rest con)))
+               (= (first con) 'ensure)
+               (assoc {} :post (vec (rest con)))
+               :else (throw (Exception. (str "Unkown tag :" (first con)))))))
+     (list* 'f args))))
+
+(defn collect-bodies [forms]
+  (for [form (partition 3 forms)]
+    (build-contract form)))
+
+(defmacro defcontract [& forms]
+  (let [name (if (symbol? (first forms))
+               (first forms)
+               nil)
+        body (if name
+               (rest forms)
+               forms)]
+    (list* 'fn name body)))
+
+
+
+(def doubler-contract ;; #1_contract_comp: Define a contract
+  (defcontract doubler
+    [x]
+    (require
+     (number? x)
+     (pos? x))
+    (ensure
+     (= (* 2 x) %))))
+
+(def times2 (partial doubler-contract #(* 2 %))) ;; #2_contract_comp: Test correct fn
